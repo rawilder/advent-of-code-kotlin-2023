cab504c82814518c82ec7b8c61f3a53509899e91
Adam Wilder <rawilder0506@gmail.com>
2023-12-01 00:45:07 -0600

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,77 @@
+import kotlin.time.measureTime
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val firstDigit = it.first(Char::isDigit)
+            val lastDigit = it.last(Char::isDigit)
+            "$firstDigit$lastDigit".toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val replacedInput = input.map { line ->
+            line.windowedSequenceVariableSize(listOf(1, 3, 4, 5))
+                .map {
+                    when {
+                        it.length == 1 && it.first().isDigit() -> it
+                        else -> Utils.digitStringsToInts[it] ?: ""
+                    }
+                }
+                .joinToString("")
+        }
+
+        return part1(replacedInput)
+    }
+
+    fun part2Optimized(input: List<String>): Int {
+        val replacedInput = input.map { line ->
+            var pos = 0
+            var result = StringBuilder()
+            while (pos < line.length) {
+                when {
+                    line[pos].isDigit() -> {
+                        result.append(line[pos])
+                    }
+                    else -> {
+                        line.substringOrNull(pos, pos + 3)?.let {
+                            Utils.digitStringsToInts[it]?.let { digit ->
+                                result.append(digit)
+                            }
+                        }
+                        line.substringOrNull(pos, pos + 4)?.let {
+                            Utils.digitStringsToInts[it]?.let { digit ->
+                                result.append(digit)
+                            }
+                        }
+                        line.substringOrNull(pos, pos + 5)?.let {
+                            Utils.digitStringsToInts[it]?.let { digit ->
+                                result.append(digit)
+                            }
+                        }
+                    }
+                }
+                pos += 1
+            }
+            result.toString()
+        }
+        return part1(replacedInput)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part1TestInput = readInput("Day01_part1_test")
+    check(part1(part1TestInput) == 142)
+
+    val part2TestInput = readInput("Day01_part2_test")
+    check(part2Optimized(part2TestInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    val part2Time = measureTime { part2(input).println() }.also {
+        println("part2: $it")
+    }
+    val part2OptimizedTime = measureTime { part2Optimized(input).println() }.also {
+        println("part2Optimized: $it")
+    }
+    println("part2Optimized is ${part2Time / part2OptimizedTime} times faster")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,46 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Returns a sequence of substrings, stepping by one character, and yielding substrings of the given sizes in the order
+ * the sizes are provided.
+ */
+fun CharSequence.windowedSequenceVariableSize(sizes: List<Int>, step: Int = 1): Sequence<String> {
+    return sequence {
+        var pos = 0
+        while (pos < length) {
+            sizes.forEach { size ->
+                if (pos + size <= length) {
+                    yield(subSequence(pos, pos + size).toString())
+                }
+            }
+            pos += step
+        }
+    }
+}
+
+/**
+ * Returns substring or null if the given indices are out of bounds.
+ */
+fun CharSequence.substringOrNull(startIndex: Int, endIndex: Int): String? {
+    return if (startIndex < 0 || endIndex > length || startIndex > endIndex) {
+        null
+    } else {
+        substring(startIndex, endIndex)
+    }
+}
+
+object Utils {
+    val digitStringsToInts = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+}
